0e7fd0e2016d5c3f3b12836b510987ab1e3bad9c
lucaruboni <lucaruboni@gmail.com>
2023-04-13 17:08:52 +0200

    initial commit [add] block layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,130 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+   
+  export default{
+
+  }
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <header>
+    <nav class="container">
+      <div class="row">
+        <div class="col">
+          <img src="./assets/img/dc-logo.png" alt="DC" class="mt-3">
+        </div>
+        <div class="col">
+          <ul class="d-flex gap-3 mt-5">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEO</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </div>
+      </div>
+
+    </nav>
+  </header>
+
+  <main>
+    <h1>     --> Your content goes here!</h1>
+  </main>
+
+  <section>
+    <div class="container"></div>
+    <div class="row mx-5">
+      <div class="col">
+        <img src="./assets/img/buy-comics-digital-comics.png" alt="buy comics" class="p-4">
+      </div>
+      
+      <div class="col">
+        <img src="./assets/img/buy-comics-merchandise.png" alt="buy comics" class="p-4">
+      </div>
+
+      <div class="col">
+        <img src="./assets/img/buy-comics-shop-locator.png" alt="buy comics" class="p-4">
+      </div>
+
+      <div class="col">
+        <img src="./assets/img/buy-comics-subscriptions.png" alt="buy comics" class="p-4">
+      </div>
+
+      <div class="col">
+        <img src="./assets/img/buy-dc-power-visa.svg" alt="buy comics" class="p-4">
+      </div>
+      
+    </div>
+  </section>
+
+  <footer>
+      <div class="row flex-wrap">
+        <div class="col-12 up_col">
+
+        </div>
+        <div class="col down_col"></div>
+      </div>
+    
+  </footer>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
+  
+  header{
+    
+    height: 150px;
+    li{
+      list-style-type: none;
+    }
+  }
+
+
+  main{
+    height: 180px;
+    background-color: black;
+    h1{
+      color: white;
+      padding-top: 2rem;
+    }
+  }
+
+
+
+  section{
+    height: 180px;
+    background-color: blue;
+    img{
+     margin-top: 2rem;
+      height: 100px;
+    }
+  }
+
+
+
+  footer{
+   
+    height: 450px;
+    
+    .row{
+    
+    .up_col{
+      height: 350px;
+      background-image: url(./assets/img/footer-bg.jpg);
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+    .down_col{
+      background-color: grey;
+      height: 100px;
+    }
+   }
+  }
+
 </style>
